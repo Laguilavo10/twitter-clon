b60ee23bcca03d5b62a6fd2b5e494f2c6db9f3e7
Andres Laguilavo <lagui2003@gmail.com>
2022-12-05 11:22:18 -0500

    maquetando componente CrearTweet

---
+++ src/components/CrearTweet/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../../styles/crearTweet.css'
+import paisaje from '../../assets/paisaje-icon.svg'
+import gif from '../../assets/gif-icon.svg'
+import encuesta from '../../assets/encuesta-icon.svg'
+import emoji from '../../assets/emoji-icon.svg'
+import calendario from '../../assets/calendario-icon.svg'
+import ubicacion from '../../assets/ubicacion-icon.svg'
+
+export  function CrearTweet() {
+  return (
+    <>
+    <section className='crear-tweet-container'>
+      <h3>Inicio</h3>
+      <div className='crear-tweet'>
+        <img src="https://i.pinimg.com/236x/ce/59/83/ce59837dd46efcaa5549a75bf2b1e443.jpg" alt="" />
+        <input type="text" placeholder='¿Que esta pasando?'/>
+      </div>
+      <div className='iconos'>
+        <img src={paisaje} alt="paisaje" />
+        <img src={gif} alt="gif" />
+        <img src={encuesta} alt="encuesta" />
+        <img src={emoji} alt="emoji" />
+        <img src={calendario} alt="calendario" />
+        <img src={ubicacion} alt="ubicacion" />
+      </div>
+      <button className='btn-twittear'>Twittear</button>
+    </section>
+    </>
+  )
+}
